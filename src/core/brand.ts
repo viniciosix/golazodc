84d3fdf660a55c1cab7459ff460ea5de7af64e1e
Vinicio <39930872+viniciosix@users.noreply.github.com>
2026-09-12 00:05:33 -0400

    feat: adicionar identidade visual Tricord

---
+++ src/core/brand.ts
@@ -0,0 +1,8 @@
+export const TRICORD_NAME = 'Tricord';
+export const TRICORD_RED = 0xe31b23;
+
+export const FOOTBALL_RESULT_EMOJIS = {
+  win: '<:vitoria:1548179277093150841>',
+  draw: '<:empate:1548178986037813410>',
+  loss: '<:derrota:1548179138869985362>',
+} as const;
